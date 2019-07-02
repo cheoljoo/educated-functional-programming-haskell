c657a5cced796e6dda5130307cbde437acd3f0a1
Charles.Lee <cheoljoo.lee@lge.com>
2019-07-03 07:57:20 +0900

    type4.hs bug fix Show -> show

--- type4.hs
+++ type4.hs
@@ -2,10 +2,10 @@ newtype Meter = Meter Double  --deriving Show   -- Meter 3
 newtype Yard = Yard Double
 
 instance Show Yard where
-     Show ( Yard y) = Show y ++ " yd"
+     show ( Yard y) = show y ++ " yd"
 
 instance Show Meter where
-    Show ( Meter m) = Show m ++ " m"
+    show ( Meter m) = show m ++ " m"
 
--- r1 = Yard 3
+r1 = Yard 3
 
